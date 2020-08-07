ef209d8b67e4fefb0aa37470645c25d1230ab2dd
humberto <humbertorivero9@gmail.com>
2020-08-07 00:18:09 -0300

    aplicacion con react

--- tareasapp/src/components/TareasBaner.js
+++ tareasapp/src/components/TareasBaner.js
@@ -2,6 +2,6 @@ import React from 'react';
 
 export const TareasBaner = props => (
     <h4 className="bg-primary text-white text center p-4">
-        {props.userName} Tareas App ({props.tareasItems.length} tareas por hacer)
+        {props.userName} Tareas App ({props.tareasItems.filter(t => !t.hecho).length} tareas por hacer)
     </h4>
 )
